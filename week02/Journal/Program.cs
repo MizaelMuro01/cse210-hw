56c0f0840289cc1a3a9d6e817df9361ed4ac6663
mizaelmuro01 <ad402349@gmail.com>
2025-09-15 14:27:13 -0500

    123

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,3 +1,9 @@
+
+
+// Creativity and Exceeding Requirements:
+// - Added a mood tracker to each journal entry. After writing, the user is asked how they are feeling.
+// - The mood is saved and displayed along with the entry.
+
 using System;
 
 class Program
